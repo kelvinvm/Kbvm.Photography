771b073ef79387d875e185b16483bc602584f5f2
Kelvin L Brostek-Van Manen <kelvin@kelvinvm.com>
2024-04-27 10:35:09 -0700

    Started on UI basics to add a photo

---
+++ Kbvm.Photography.Blazor/Components/Pages/PhotoInfo.razor.cs
@@ -0,0 +1,29 @@
+﻿using Kbvm.Photograph.Shared.Models;
+
+namespace Kbvm.Photography.Blazor.Components.Pages
+{
+	public partial class PhotoInfo
+	{
+		private List<PhotoDto> _photos = [];
+		private PhotoDto _newPhoto = new();
+
+		private List<string> _collections =
+		[
+			"All Time Favorites",
+			"Astoria, OR",
+			"Bainbridge Island, 2022",
+			"Cherry Blossoms",
+			"Darcell XV, July, 2022",
+			"Europe, 2018",
+			"Keukenhof, Netherlands, 2018",
+			"Sachsenhausen",
+			"San Francisco, 2023",
+			"Seattle, 2022",
+		];
+
+		private void Submit()
+		{
+
+		}
+	}
+}
